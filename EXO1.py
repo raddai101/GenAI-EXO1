cb9649bc9bac35ebcfed28ba2810ecea64ac199f
raddai101 <nkashamaradda.arc@gmail.com>
2026-06-16 14:33:24 +0200

    premier commit

---
+++ EXO1.py
@@ -0,0 +1,30 @@
+import random
+
+
+
+def nombre_mystere():
+    
+    print("Bienvenue dans le jeu du nombre mystère !")
+    
+    nombre_mystere = random.randint(1, 100)
+    essais = 0
+    while True:
+        try:
+            essais += 1
+            nombre_utilisateur = int(input("Entrez un nombre entre 1 et 100 : "))
+            
+            
+            if nombre_utilisateur < nombre_mystere:
+                print("Le nombre mystère est plus grand.")
+            elif nombre_utilisateur > nombre_mystere:
+                print("Le nombre mystère est plus petit.")
+            elif nombre_utilisateur < 1 or nombre_utilisateur > 100:
+                print("Veuillez entrer un nombre valide entre 1 et 100.")
+            elif nombre_utilisateur == nombre_mystere:
+                print(f"Bravo ! Vous avez trouvé le nombre mystère en {essais} essais.")
+        except ValueError:
+            print("Veuillez entrer un nombre valide.")   
+        
+nombre_mystere()
+# if "___name__" == "__main__":
+#     nombre_mystere()
